a890e83c1d23223135298205390361061a5d54cb
ThabeloPeter <thabelopetertshivhidzo@gmail.com>
2021-09-07 16:43:56 +0200

    0-p-n-z

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,7 +18,7 @@ int main(void)
 
 	if (n>0)
 	{
-		printf("%i is negative\n", n);
+		printf("%i is positive\n", n);
 	}
 	else if (n==0)
 	{
@@ -26,7 +26,7 @@ int main(void)
 	}
 	else
 	{
-		printf("%i is positive\n", n);
+		printf("%i is negative\n", n);
 	}
 	return(0);	
 }
